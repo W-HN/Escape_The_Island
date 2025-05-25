e56a6285d6aefb4fbfcdb675f195a46588102bcf
William Hjerde <williamhjerden@gmail.com>
2025-05-25 14:46:39 +0200

    added player2 sprites and cuttable bushes

---
+++ scripts/bush.gd
@@ -0,0 +1,23 @@
+extends Area2D  # or Area2D if you want collision
+
+@export var bush: Texture2D = preload("res://assets/objects/bush.png")
+@export var bush_cut: Texture2D = preload("res://assets/objects/bush_cut.png")
+
+var is_cut = false
+
+func _ready():
+	# Set to the whole bush at start
+	$Sprite2D.texture = bush
+
+func cut_bush():
+	if not is_cut:
+		is_cut = true
+		$Sprite2D.texture = bush_cut
+		$Sprite2D.z_index = -1
+		
+
+func _on_area_entered(area: Area2D) -> void:
+	print("entered")
+	if area.is_in_group("attack"):
+		print("cut")
+		cut_bush()
